35daf64af53570d99c4ec95fdb6e7a71ba880183
Nuno Farinha <nadfarinha@gmail.com>
2019-03-28 13:45:03 +0000

    Card set up complete with tests

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+
 public class Card {
+
+    private SuitType suit;
+    private RankType rank;
+
+    public Card(SuitType suit, RankType rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public SuitType suit() {
+        return this.suit;
+    }
+
+    public RankType rank() {
+        return this.rank;
+    }
+
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+
+    Card card;
+
+    @Before
+
+    public void before() {
+        card = new Card(SuitType.CLUBS, RankType.ACE);
+
+    }
+    @Test
+    public void hasSuit() {
+        assertEquals(SuitType.CLUBS, card.suit());
+
+    }
+    @Test
+    public void hasRank() {
+        assertEquals(RankType.ACE, card.rank());
+    }
 }
