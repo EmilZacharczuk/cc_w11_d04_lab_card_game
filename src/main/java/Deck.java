9604a70d8140799a912da89fdedff4d64df7c1b5
Nuno Farinha <nadfarinha@gmail.com>
2019-03-28 15:33:59 +0000

    Create suffle cards function

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Deck {
     private ArrayList<Card> cards;
@@ -9,5 +9,52 @@ public class Deck {
     }
     public int getCount(){
         return this.cards.size();
+    }
+
+    public Card createCard(SuitType suit, RankType rank) {
+        Card newCard = new Card(suit, rank);
+        return newCard;
+    }
+
+    public void populateCards() {
+        SuitType[] suits = SuitType.values();
+        RankType[] ranks = RankType.values();
+        for (int i = 0; i < suits.length; i++) {
+            for (int j = 0; j < ranks.length; j++) {
+                Card newCard = createCard(suits[i], ranks[j]);
+                this.cards.add(newCard);
+            }
+        }
+    }
+
+    public ArrayList<Card> getCards() {
+        return this.cards;
+    }
+
+    public void shuffleCards() {
+        List<Card> solution = this.cards;
+        for(int i = 0; i < 6; i++) {
+            solution.add(this.cards.get(i));
+        }
+        Collections.shuffle(solution);
+
+
+        /*
+        public static int[] RandomizeArray(int[] array){
+        Random rgen = new Random();  // Random number generator
+
+        for (int i=0; i<array.length; i++) {
+            int randomPosition = rgen.nextInt(array.length);
+            int temp = array[i];
+            array[i] = array[randomPosition];
+            array[randomPosition] = temp;
+        }
+
+        return array;
+    }
+        * */
+
+
+
     }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -2,9 +2,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class DeckTest {
     Deck deck;
+
 
 
     @Before
@@ -15,5 +17,33 @@ public class DeckTest {
     @Test
     public void emptyCardsArray() {
         assertEquals(0, deck.getCount());
+    }
+
+    @Test
+    public void canCreateCard() {
+        Card testCard = deck.createCard(SuitType.HEARTS, RankType.ACE);
+        assertEquals(SuitType.HEARTS, testCard.suit());
+        assertEquals(RankType.ACE, testCard.rank());
+    }
+
+    @Test
+    public void canPopulateCardDeck(){
+        deck.populateCards();
+        assertEquals(52, deck.getCount());
+    }
+
+    @Test
+    public void canGetCards() {
+        deck.populateCards();
+        assertEquals(52, deck.getCards().size());
+    }
+
+    @Test
+    public void canShuffleCards(){
+        deck.populateCards();
+        Card actualCard = deck.getCards().get(0);
+        deck.shuffleCards();
+        Card expectedCard = deck.getCards().get(0);
+        assertNotEquals(expectedCard.rank(), actualCard.rank());
     }
 }
