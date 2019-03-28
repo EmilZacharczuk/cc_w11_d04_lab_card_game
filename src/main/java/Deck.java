4368eec56730842e5638a97fd2da6ac0e67f1425
Nuno Farinha <nadfarinha@gmail.com>
2019-03-28 13:56:27 +0000

    Done deck and tested

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,3 +1,13 @@
-public class Deck {
+import java.util.ArrayList;
 
+public class Deck {
+    private ArrayList<Card> cards;
+
+
+    public Deck() {
+        this.cards = new ArrayList<>();
+    }
+    public int getCount(){
+        return this.cards.size();
+    }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+    Deck deck;
+
+
+    @Before
+    public void before(){
+        this.deck = new Deck();
+    }
+
+    @Test
+    public void emptyCardsArray() {
+        assertEquals(0, deck.getCount());
+    }
 }
